4d0bac741e22ce4200fb0535245882d1a8f44329
Akshar-mishra <mishraakshar71@gmail.com>
2026-02-10 13:25:54 +0000

    conversion

---
+++ BASICS_1/conversion_op.js
@@ -0,0 +1,16 @@
+let score=33  
+
+console.log(typeof score);
+
+let valueInNum= Number(score)
+console.log(typeof valueInNum);
+
+// "33abc"=>NaN  
+// null=>0 
+// undef=>NaN
+// ture=>1
+
+let isLogin=1
+let checkLogin=Boolean(isLogin)
+console.log(checkLogin);
+
