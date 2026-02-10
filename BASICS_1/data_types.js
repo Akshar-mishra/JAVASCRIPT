e950e0bb17673347dca7f7653d32551619ce2b25
Akshar-mishra <mishraakshar71@gmail.com>
2026-02-10 12:02:53 +0000

    var and dtt

---
+++ BASICS_1/data_types.js
@@ -0,0 +1,13 @@
+ /*
+ number=>    2^53
+ string=     ""
+ bool=>      true/false
+ null=>      standalone value
+ undefined=> not assigned
+ symbol=>    to identify unqieness
+ object
+ */
+let namee="akshar"
+console.log(typeof namee)
+console.log(typeof null)   // null is an obj
+console.log(typeof undefined)
---
+++ BASICS_1/variables.js
@@ -0,0 +1,17 @@
+const accId=1442
+let accEmail="akshar@gmail.com"
+var accPAss="1548"
+accCity="jbp"
+let accState;
+
+//accId=121 NOT allowed
+console.log(accId);
+
+accEmail="he@gmail"
+accPAss="21"
+accCity="indore"
+
+console.table([accId,accEmail,accPAss,accCity,accState])
+
+
+
