a6927b60a5a466b6fd1bef3e5d275d24087ef392
Akshar-mishra <mishraakshar71@gmail.com>
2026-02-11 06:53:25 +0000

    string

---
+++ BASICS_1/string.js
@@ -0,0 +1,25 @@
+const name="akshar "
+const repocount=25
+console.log(name + repocount);
+console.log(`my name is ${name} and my repo is ${repocount}`) //good way
+
+const gamename= new String('CODMW-I-I')    // modern way of str paste in console (try)
+//console.log(gamename);
+console.log(gamename.split('-'));
+//console.log(gamename[2]);
+console.log(gamename.charAt(3));
+console.log(gamename.indexOf('M'));
+console.log(gamename.toLowerCase());
+
+const newgame=gamename.substring(0,4)
+console.log(newgame);
+
+const another=gamename.slice(-2,4)
+console.log(another);
+
+//trim
+//replace
+
+
+
+
