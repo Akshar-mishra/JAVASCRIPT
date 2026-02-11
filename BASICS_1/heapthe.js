0b8f5a0afa286679636276124c236ad79f8e4b57
Akshar-mishra <mishraakshar71@gmail.com>
2026-02-11 06:30:28 +0000

    concepts

---
+++ BASICS_1/heapthe.js
@@ -0,0 +1,16 @@
+// stack(premitive) , 
+let accid="akshar@gmail.com"
+let id=accid
+id="kk@gamil.com"
+console.log(id);
+console.log(accid);
+
+//heap(non-premitive)
+let user1={
+    name:"akshar",
+    id:"krissh@gmail.com"
+}
+let user2=user1
+user2.name="kk"
+console.log(user1.name);
+console.log(user2.name);
